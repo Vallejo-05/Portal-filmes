902f263ad6a5bac0f182bc3da2d3beafb6a77770
Vallejo-05 <84139425+Vallejo-05@users.noreply.github.com>
2024-09-17 20:57:04 -0300

    Add files via upload

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,12 @@ import { NavLink } from "react-router-dom"
 export default function Header(){
     return(
         <>
-            <header>
+            <header className="flex w-full h-14 bg-purple-700 justify-around items-center text-white">
                 <div>
                     <h1>Portal Filmes</h1>
                 </div>
                 <nav>
-                    <ul>
+                    <ul className="flex gap-6">
                         <li><NavLink to="/">Home</NavLink></li>
                         <li><NavLink to="/movies">Lista dos filmes</NavLink></li>
                         <li><NavLink to="/genre">Lista de Gênero</NavLink></li>
