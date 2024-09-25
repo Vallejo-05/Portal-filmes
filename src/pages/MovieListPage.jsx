d45b9b742353fa5122c3745a550e40b3ff2bbd61
Vallejo-05 <84139425+Vallejo-05@users.noreply.github.com>
2024-09-24 21:26:38 -0300

    Add files via upload

--- src/pages/MovieListPage.jsx
+++ src/pages/MovieListPage.jsx
@@ -1,10 +1,62 @@
+import { useState } from "react";
+import movies from "../data/movies.json";
+import MovieCard from "../components/MovieCard";
+
 export default function MovieListPage(){
+{
+    /*
+    const [contador, setContador] = useState(1100)
+    const [textoBotao, setTextoBotao] = useState("Memphis")
+
+    const handleClick = () => {
+        setContador((count) => count + 1)
+        console.log(contador)
+    }
+
+    const handleClick2 = () => {
+        setContador((count) => count - 1)
+        console.log(contador)
+    }
+
+    const handleClick0 = () => {
+        setContador(0)
+    }
+
+    const handleBotao = () =>{
+        setTextoBotao(count =>(
+            count === "Memphis" ? "Depay" : "Memphis"
+        ))
+    }
+*/}
+    const [search, setSearch] = useState('')
+    const [listaFIlmes, SetListaFilmes] = useState(movies)
+
+    const handleSearch = (e) =>{
+        setSearch(e.target.value)
+        SetListaFilmes(filmesFiltrados)
+    }
+
     return(
         <>
-        <h1>Movie List Page</h1>
-        {/* Exibe a lista de todos os filmes disponíveis categorizados por gênero.
-        Cada filme na lista é mostrado com uma imagem, 
-        título e um link para a página de detalhes do filme.*/}
+        <h2>Veja a lista completa de filmes</h2>
+        <input className="text-black" type="text" name="search" id="search" value={search} onChange={handleSearch}/>
+
+        <div className="flex">
+        {
+            listaFIlmes
+            .filter( filme => (filme.titulo).includes(search))
+            .map(movie =>(
+                <MovieCard key={movie.id} {...movie}/>
+            ))
+        }
+        </div>
+        {/*<p>{Math.random() * 101}</p>
+        <p>{contador}</p>
+        <button onClick={handleClick}>Aumentar</button>
+        <button onClick={handleClick2}>Diminuir</button>
+        <button onClick={handleClick0}>Zerar</button>
+        <button onClick={handleBotao}>{textoBotao}</button>
+        */}
         </>
     )
 }
